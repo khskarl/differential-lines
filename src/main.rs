d04c07a842ff63b575bbe8646a4113acdb62f443
khskarl <karllhenning64@gmail.com>
2019-06-19 16:31:14 -0300

    Refactored successfully

--- src/main.rs
+++ src/main.rs
@@ -9,45 +9,25 @@ struct Model {
     ps: ParticleSystem,
 }
 
-struct Particle {
-    position: Point2,
-    color: Rgba<f32>,
-}
-
-impl Particle {
-    fn new(position: Vector2) -> Self {
-        let l = random_f32() * 0.8 + 0.1;
-        let color = Rgba::new(l, l - random_f32() * 0.2, l - random_f32() * 0.1, 1.0);
-
-        Particle { position, color }
-    }
-
-    fn update(&mut self) {
-        self.position += vec2(random_f32() - 0.5, random_f32() - 0.5) * 2.0;
-    }
-
-    fn display(&self, draw: &app::Draw) {
-        let size = 4.0;
-
-        draw.ellipse().xy(self.position).w_h(size, size).rgba(
-            self.color.red,
-            self.color.green,
-            self.color.blue,
-            self.color.alpha,
-        );
-    }
-}
-
 struct ParticleSystem {
-    particles: Vec<Particle>,
+    num_particles: usize,
+    positions: Vec<Point2>,
+    colors: Vec<Rgba<f32>>,
     edges: Vec<(usize, usize)>,
 }
 
 impl ParticleSystem {
     fn new() -> Self {
-        let particles = Vec::new();
+        let positions = Vec::new();
+        let colors = Vec::new();
         let edges = Vec::new();
-        ParticleSystem { particles, edges }
+
+        ParticleSystem {
+            num_particles: 0,
+            positions,
+            colors,
+            edges,
+        }
     }
 
     fn spawn_particles(&mut self, num_particles: usize) {
@@ -58,7 +38,12 @@ impl ParticleSystem {
             let direction = vec2(phi.cos(), phi.sin());
             let position = direction * 100.0;
 
-            self.particles.push(Particle::new(position));
+            let l = random_f32() * 0.8 + 0.1;
+            let color = Rgba::new(l, l - random_f32() * 0.2, l - random_f32() * 0.1, 1.0);
+
+            self.positions.push(position);
+            self.colors.push(color);
+
             let prev_particle = {
                 if i as i32 - 1 < 0 {
                     num_particles - 1
@@ -79,36 +64,37 @@ impl ParticleSystem {
 
             phi += delta_phi;
         }
+
+        self.num_particles = num_particles;
     }
 
     fn update(&mut self) {
-        for i in (0..self.particles.len()).rev() {
+        for i in 0..self.num_particles {
             let neighbors = self.get_neighbors_of_particle(i);
 
             let brotherhood_center = {
                 let (b0, b1) = self.edges[i];
-                (self.particles[b0].position + self.particles[b1].position) / 2.0
+                (self.positions[b0] + self.positions[b1]) / 2.0
             };
-            let direction_to_brotherhood = brotherhood_center - self.particles[i].position;
-            self.particles[i].position += direction_to_brotherhood.normalize() * 0.5;
+            let direction_to_brotherhood = brotherhood_center - self.positions[i];
+            self.positions[i] += direction_to_brotherhood.normalize() * 0.5;
 
             let hateship_center = {
                 let quantity = neighbors.len() as f32;
                 let mut center = vec2(0.0, 0.0);
                 for j in neighbors {
-                    center += self.particles[j].position;
+                    center += self.positions[j];
                 }
                 center / quantity.max(1.0)
             };
 
-            let direction_from_hate = self.particles[i].position - hateship_center;
-            self.particles[i].position += direction_from_hate.normalize() * 0.3;
+            let direction_from_hate = self.positions[i] - hateship_center;
+            self.positions[i] += direction_from_hate.normalize() * 0.3;
 
-            // self.particles[i].update();
             {
                 let (b0, b1) = self.edges[i];
-                let (pos_b0, pos_b1) = (self.particles[b0].position, self.particles[b1].position);
-                let pos_i = self.particles[i].position;
+                let (pos_b0, pos_b1) = (self.positions[b0], self.positions[b1]);
+                let pos_i = self.positions[i];
 
                 let to_b0 = (pos_b0 - pos_i).normalize();
                 let to_b1 = (pos_b1 - pos_i).normalize();
@@ -116,31 +102,35 @@ impl ParticleSystem {
 
                 let tolerance = 0.05;
                 if to_b0.dot(to_center).abs() < tolerance && random_f32() < 0.05 {
-                    self.particles[b0].color = Rgba::new(0.2, 0.3, 1.0, 1.0);
-                    self.particles[i].color = Rgba::new(0.2, 0.3, 1.0, 1.0);
+                    self.colors[b0] = Rgba::new(0.2, 0.3, 1.0, 1.0);
+                    self.colors[i] = Rgba::new(0.2, 0.3, 1.0, 1.0);
                     self.split_at(b0, i);
                 } else {
-                    self.particles[b0].color = Rgba::new(1.0, 0.3, 0.2, 1.0);
-                    self.particles[i].color = Rgba::new(1.0, 0.3, 0.2, 1.0);
+                    self.colors[b0] = Rgba::new(1.0, 0.3, 0.2, 1.0);
+                    self.colors[i] = Rgba::new(1.0, 0.3, 0.2, 1.0);
                 }
 
                 if to_b1.dot(to_center).abs() < tolerance && random_f32() < 0.05 {
-                    self.particles[i].color = Rgba::new(0.2, 0.3, 1.0, 1.0);
-                    self.particles[b1].color = Rgba::new(0.2, 0.3, 1.0, 1.0);
+                    self.colors[i] = Rgba::new(0.2, 0.3, 1.0, 1.0);
+                    self.colors[b1] = Rgba::new(0.2, 0.3, 1.0, 1.0);
                     self.split_at(i, b1);
                 } else {
-                    self.particles[i].color = Rgba::new(1.0, 0.3, 0.2, 1.0);
-                    self.particles[b1].color = Rgba::new(1.0, 0.3, 0.2, 1.0);
+                    self.colors[i] = Rgba::new(1.0, 0.3, 0.2, 1.0);
+                    self.colors[b1] = Rgba::new(1.0, 0.3, 0.2, 1.0);
                 }
             }
         }
     }
 
     fn split_at(&mut self, p0: usize, p1: usize) {
-        let new_index = self.particles.len();
-        let new_pos = (self.particles[p0].position + self.particles[p1].position) / 2.0;
-        self.particles.push(Particle::new(new_pos));
+        let new_index = self.positions.len();
+        let new_pos = (self.positions[p0] + self.positions[p1]) / 2.0;
+        let new_color = (self.colors[p0] + self.colors[p1]) / 2.0;
+
+        self.positions.push(new_pos);
+        self.colors.push(new_color);
         self.edges.push((p0, p1));
+        self.num_particles += 1;
 
         self.edges[p0].1 = new_index;
         self.edges[p1].0 = new_index;
@@ -149,13 +139,12 @@ impl ParticleSystem {
     fn get_neighbors_of_particle(&self, index: usize) -> Vec<usize> {
         let mut neighbors = Vec::<usize>::new();
 
-        for j in 0..self.particles.len() {
+        for j in 0..self.num_particles {
             if index == j || j == self.edges[index].0 || j == self.edges[index].1 {
                 continue;
             }
 
-            let distance =
-                (self.particles[index].position - self.particles[j].position).magnitude();
+            let distance = (self.positions[index] - self.positions[j]).magnitude();
 
             let radius = 12.0;
             if distance <= radius {
@@ -170,23 +159,24 @@ impl ParticleSystem {
         let thickness = 0.1;
 
         for i in 0..self.edges.len() {
-            let (prev, next) = self.edges[i];
+            let (_, next) = self.edges[i];
 
             draw.line()
-                .start(self.particles[prev].position)
-                .end(self.particles[i].position)
-                .thickness(thickness)
-                .rgba(0.8, 0.8, 0.8, 0.1);
-
-            draw.line()
-                .start(self.particles[i].position)
-                .end(self.particles[next].position)
+                .start(self.positions[i])
+                .end(self.positions[next])
                 .thickness(thickness)
                 .rgba(0.8, 0.8, 0.8, 0.1);
         }
 
-        for p in self.particles.iter() {
-            p.display(&draw);
+        for i in 0..self.num_particles {
+            let size = 4.0;
+
+            draw.ellipse().xy(self.positions[i]).w_h(size, size).rgba(
+                self.colors[i].red,
+                self.colors[i].green,
+                self.colors[i].blue,
+                self.colors[i].alpha,
+            );
         }
     }
 }
